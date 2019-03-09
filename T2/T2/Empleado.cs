22bb885b5bc8effce440fd608c548fc98bc74258
Pikeru <47680246+Pikeru@users.noreply.github.com>
2019-03-08 18:05:33 -0800

    Ooof

---
+++ T2/T2/Empleado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    public class Empleado
+    {
+
+        private string nombre;
+        private string puesto;
+        private string diasdisponibles;
+        private string horasdisponibles;
+        private int nivelingles;
+        public string Nombre { get; set; }
+        public string Puesto { get; set; }
+        public string Diasdisponibles { get; set; }
+        public string Horasdisponibles { get; set; }
+        public int Nivelingles { get; set; }
+        public void Ra()
+        {
+            Console.Write("Ingrese su nombre: ");
+            Nombre = Console.ReadLine();
+            Console.Write("Ingrese su puesto: ");
+            Puesto = Console.ReadLine();
+            Console.Write("Ingrese los dias disponibles: ");
+            Diasdisponibles = Console.ReadLine();
+            Console.Write("Ingrese las horas en las que esta disponible: ");
+            Horasdisponibles = Console.ReadLine();
+            Console.Write("Ingrese su nivel de ingles(En valores numericos): ");
+            Nivelingles = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ T2/T2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0, r=0;
+            char n;
+            Empleado objeto = new Empleado();
+            do
+            {
+                objeto.Ra();
+                Console.WriteLine("Continue?      Y/N");
+                do {
+                    n = Convert.ToChar(Console.ReadLine());
+                    if (n == 'Y')
+                    {
+                        Console.WriteLine("");
+                        r++;
+                    }
+                    else if (n == 'N')
+                    {
+                        r++;
+                        k++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tecla equivocada");
+                    }
+                }
+                while (r<=0);
+            }
+            while (k <= 0);
+            Console.WriteLine("Vuelva pronto");
+            Console.ReadKey();
+            
+        }
+    }
+}
